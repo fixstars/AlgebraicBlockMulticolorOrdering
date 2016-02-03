afe5c6bda0a6398cc408743eccc011b3ade68b9f
LWisteria <lwisteria.ao@gmail.com>
2016-02-03 14:38:16 +0900

    出力処理を共通化

--- Abmc/main.cpp
+++ Abmc/main.cpp
@@ -8,38 +8,14 @@ static constexpr std::size_t n = 8;
 static constexpr std::size_t N = n*n;
 
 using Matrix = boost::numeric::ublas::compressed_matrix<double>;
+using Index = std::remove_const_t<decltype(N)>;
+using Color = Index;
 
 static constexpr std::size_t MAX_COLOR_COUNT = 27;
 static constexpr std::size_t BLOCK_SIZE = 2; // 2x2にブロック化
 
-
-// 幾何形状を用いた多色順序付け
-static void GeometicMultiColoring(const Matrix& A)
+static void OutputResult(const std::string name, const Matrix& A, const Color color[])
 {
-	using Index = std::remove_const_t<decltype(N)>;
-	using Color = Index;
-
-	auto color = std::make_unique<Color[]>(N);
-
-	for(auto i = decltype(n)(0); i < n; i++)
-	{
-		for(auto j = decltype(n)(0); j < n; j++)
-		{
-			const auto idx = i*n + j;
-
-			const auto isEvenRow    = i % 2 == 0;
-			const auto isEvenColumn = j % 2 == 0;
-
-			color[idx] = isEvenRow ?
-				(isEvenColumn ?
-					Color(1) :  // 偶数行の偶数列
-					Color(2)) : // 偶数行の奇数列
-				(isEvenColumn ?
-					Color(3) :  // 奇数業の偶数列
-					Color(4));  // 奇数業の奇数列
-		}
-	}
-
 	// 並び替え後の行番号→元の行番号の変換表
 	auto row = std::make_unique<Index[]>(N);
 	{
@@ -104,7 +80,7 @@ static void GeometicMultiColoring(const Matrix& A)
 		}
 	}
 
-	std::cout << "幾何的多色順序付け" << std::endl;
+	std::cout << name << std::endl;
 	for(auto i = decltype(N)(0); i < N; i++)
 	{
 		for(auto j = decltype(N)(0); j < N; j++)
@@ -123,11 +99,36 @@ static void GeometicMultiColoring(const Matrix& A)
 	}
 }
 
+// 幾何形状を用いた多色順序付け
+static void GeometicMultiColoring(const Matrix& A)
+{
+	auto color = std::make_unique<Color[]>(N);
+
+	for(auto i = decltype(n)(0); i < n; i++)
+	{
+		for(auto j = decltype(n)(0); j < n; j++)
+		{
+			const auto idx = i*n + j;
+
+			const auto isEvenRow    = i % 2 == 0;
+			const auto isEvenColumn = j % 2 == 0;
+
+			color[idx] = isEvenRow ?
+				(isEvenColumn ?
+					Color(1) :  // 偶数行の偶数列
+					Color(2)) : // 偶数行の奇数列
+				(isEvenColumn ?
+					Color(3) :  // 奇数業の偶数列
+					Color(4));  // 奇数業の奇数列
+		}
+	}
+
+	OutputResult("幾何的多色順序付け", A, color.get());
+}
+
 // 幾何形状を用いない多色順序付け
 static void AlgebraicMultiColoring(const Matrix& A)
 {
-	using Index = std::remove_const_t<decltype(N)>;
-	using Color = Index;
 	auto inNeighbor = std::array<bool, MAX_COLOR_COUNT>();
 	auto color = std::make_unique<Color[]>(N);
 	constexpr Color INVALID_COLOR = 0;
@@ -162,87 +163,7 @@ static void AlgebraicMultiColoring(const Matrix& A)
 		}
 	}
 
-	// 並び替え後の行番号→元の行番号の変換表
-	auto row = std::make_unique<Index[]>(N);
-	{
-		// 色番号の小さい順に並び替え（同じ色の場合は行番号の小さい順）
-		auto pair = std::make_unique<std::pair<Color, Index>[]>(N);
-		for(auto i = decltype(N)(0); i < N; i++)
-		{
-			pair[i].first = color[i];
-			pair[i].second = i;
-		}
-		std::sort(pair.get(), pair.get() + N, [](auto left, auto right)
-		{
-			return (left.first != right.first) ? (left.first < right.first) : (left.second < right.second);
-		});
-		const auto colorCount = pair[n - 1].first; // 最後の色が色番号最大のはず
-
-		// 行番号だけの配列に変換
-		std::transform(pair.get(), pair.get() + N, row.get(), [](const auto p)
-		{
-			return p.second;
-		});
-	}
-
-	// 元の行番号→並び替え後の行番号の変換表
-	auto lut = std::make_unique<Index[]>(N);
-	{
-		// 並び替え後の行番号順に
-		auto pair = std::make_unique<std::pair<Index, Index>[]>(N);
-		for(auto i = decltype(N)(0); i < N; i++)
-		{
-			pair[i].first = row[i];
-			pair[i].second = i;
-		}
-		std::sort(pair.get(), pair.get() + N, [](auto left, auto right)
-		{
-			return (left.first < right.first);
-		});
-
-		// 変換表
-		std::transform(pair.get(), pair.get() + N, lut.get(), [](const auto p)
-		{
-			return p.second;
-		});
-	}
-
-	Matrix B(N, N);
-	for(auto i = decltype(N)(0); i < N; i++)
-	{
-		const auto r = row[i];
-
-		const auto offset = A.index1_data()[r];
-		const auto count = A.index1_data()[r + 1] - offset;
-
-		// 接続している点に使われている色を洗い出す
-		for(auto idx = decltype(count)(0); idx < count; idx++)
-		{
-			const auto j = A.index2_data()[offset + idx];
-			const auto value = A.value_data()[offset + idx];
-
-			const auto c = lut[j];
-			B(i, c) = value;
-		}
-	}
-
-	std::cout << "代数的多色順序付け" << std::endl;
-	for(auto i = decltype(N)(0); i < N; i++)
-	{
-		for(auto j = decltype(N)(0); j < N; j++)
-		{
-			std::cout << B(i, j) << ", ";
-		}
-		std::cout << std::endl;
-	}
-	for(auto i = decltype(n)(0); i < n; i++)
-	{
-		for(auto j = decltype(n)(0); j < n; j++)
-		{
-			std::cout << color[i*n + j] << ", ";
-		}
-		std::cout << std::endl;
-	}
+	OutputResult("代数的多色順序付け", A, color.get());
 }
 
 int main()
